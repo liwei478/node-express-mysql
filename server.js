17592075ee0f106d51ba62a3067891e693c94767
lwz <lwz@example.com>
2023-10-22 22:29:14 +0800

    feat: init project

---
+++ server.js
@@ -0,0 +1,26 @@
+import express from "express";
+import cors from 'cors'
+
+const app = express()
+
+const corsOptions = {
+  origin: 'http://localhost:8081'
+}
+
+// config CORS to ensure that your Express server is secure and only allows trusted origins to make requests.
+// app.use method is used to apply CORS middleware to your Express applciation.
+app.use(cors(corsOptions))
+
+// parse request of content-type - application/json
+app.use(express.json())
+
+//simple route
+app.get('/', (req, res) => {
+  res.json({message: 'Welcome t bezkoder application.'})
+})
+
+// set port, listen for requests
+const PORT = process.env.PORT | 8080
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`)
+})
