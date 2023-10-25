553e845c5a25ac9d97d482fe705e03783d93cf74
lwz <lwz@example.com>
2023-10-25 23:30:31 +0800

    feat: add router

--- app/routes/router.js
+++ app/routes/router.js
@@ -1,14 +1,32 @@
 import express from "express";
+import tutorials from "../controllers/tutorial.controller";
 
 const app = express()
 
 const router = express.Router()
 
-router.get('/', (req, res) => {
-  res.json({message: 'Welcome bezkoder application.'})
-})
+// Create a new Tutorial
+router.post('/', tutorials.create)
+
+// Retrieve all Tutorials
+router.get('/', tutorials.findAll)
+
+// Retrieve all published Tutorials
+router.get('/published', tutorials.findAllPulished)
+
+// Retrieve a single Tutorial with id
+router.get('/:id', tutorials.findOne)
+
+// Update a Tutorial with id
+router.put('/:id', tutorials.update)
+
+// Delete a Tutorial with id
+router.delete('/:id', tutorials.deleteById)
+
+// Delete all Tutorials
+router.delete('/:id', tutorials.deleteAll)
 
 
 export default app => {
-  app.use('/', router)
+  app.use('/api/tutorials', router)
 }
